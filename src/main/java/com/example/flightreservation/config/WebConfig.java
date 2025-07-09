034835949b5d2d97def3cc1e6ea9df23573c0adc
Guy-Ghis <tissongguyghislain001@gmail.com>
2025-07-09 07:34:20 +0100

    Add WebConfig class to configure CORS settings for the application

---
+++ src/main/java/com/example/flightreservation/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.flightreservation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
